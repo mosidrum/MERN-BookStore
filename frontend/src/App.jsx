5fcf0145cf802021ed64d419a5f546712b3c36f4
mosidrum <mosiokanga@gmail.com>
2023-12-05 13:43:53 +0100

    Add pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import Home from "./Pages/Home.jsx";
+import ShowBook from "./Pages/ShowBook.jsx";
+import EditBook from "./Pages/EditBook.jsx";
+import CreateBook from "./Pages/CreateBook.jsx";
+import DeleteBook from "./Pages/DeleteBook.jsx";
 
-const App = () => <div className='bg-red-400 text-white'>App</div>;
+const App = () => (
+  <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/books/details/:id' element={<ShowBook />} />
+    <Route path='/books/edit/:id' element={<EditBook />} />
+    <Route path='/books/create' element={<CreateBook />} />
+    <Route path='/books/delete/:id' element={<DeleteBook />} />
+  </Routes>
+);
 
 export default App;
