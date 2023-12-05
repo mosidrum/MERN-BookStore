670d8f26d7f8b12bc1edaba374454bcf7fdba5ad
mosidrum <mosiokanga@gmail.com>
2023-12-05 11:33:47 +0100

    connect mongo db

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 8888;
+
+export const mongoUrl = "mongodb+srv://root:root@book-store-mern.t4evsik.mongodb.net/books-collection?retryWrites=true&w=majority"
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoUrl } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`App is listening on ${PORT}`);
+app.get('/', (req, res) => {
+  console.log(req);
+  return res.status(234).send('Welcome to MERN stack bookstore');
 });
+
+mongoose.connect(mongoUrl).then(() => {
+  console.log('App connected to database');
+  app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}`);
+  });
+  
+}).catch((err) => {
+  console.log(err);
+})
+
+// add a book
