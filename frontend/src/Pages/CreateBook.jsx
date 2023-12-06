185732e33cf2bd84d8e75aacf3b5d9677bbf4962
mosidrum <mosiokanga@gmail.com>
2023-12-06 04:15:37 +0100

    DRY practice

--- frontend/src/Pages/CreateBook.jsx
+++ frontend/src/Pages/CreateBook.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 import axios from 'axios';
@@ -6,72 +6,77 @@ import { useNavigate } from 'react-router-dom';
 import { useSnackbar } from 'notistack';
 
 const CreateBook = () => {
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const [publishYear, setPublishYear] = useState('');
+  const [formData, setFormData] = useState({
+    title: '',
+    author: '',
+    publishYear: '',
+  });
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const { enqueueSnackbar } = useSnackbar();
 
- const handleCreateBook = () => {
-  const data = {
-    title,
-    author,
-    publishYear
-  }
-  setLoading(true);
-  axios
-    .post('http://localhost:8888/books', data)
-    .then(() => {
-      setLoading(false);
-      enqueueSnackbar('Book created successfully', { variant: 'success'});
-      navigate('/');
-    })
-    .catch((error) => {
-      console.log(error);
-      enqueueSnackbar('Error', { variant: 'error'});
-    })
- }
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleCreateBook = () => {
+    setLoading(true);
+    axios
+      .post('http://localhost:8888/books', formData)
+      .then(() => {
+        setLoading(false);
+        enqueueSnackbar('Book created successfully', { variant: 'success' });
+        navigate('/');
+      })
+      .catch((error) => {
+        console.log(error);
+        enqueueSnackbar('Error', { variant: 'error' });
+      });
+  };
 
   return (
     <div className='p-4'>
       <BackButton />
-      { loading ? <Spinner /> : ''}
+      {loading && <Spinner />}
       <h1 className='text-3xl my-4 text-center'>Create book</h1>
       <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
         <div className='my-4'>
-          <label className="text-xl mr-4 text-gray-500">Title</label>
+          <label className='text-xl mr-4 text-gray-500'>Title</label>
           <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            type='text'
+            name='title'
+            value={formData.title}
+            onChange={handleInputChange}
             className='border-2 border-gray-500 px-4 py-2 w-full'
-           />
+          />
         </div>
         <div className='my-4'>
-          <label className="text-xl mr-4 text-gray-500">Author</label>
+          <label className='text-xl mr-4 text-gray-500'>Author</label>
           <input
-            type="text"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
+            type='text'
+            name='author'
+            value={formData.author}
+            onChange={handleInputChange}
             className='border-2 border-gray-500 px-4 py-2 w-full'
-           />
+          />
         </div>
         <div className='my-4'>
-          <label className="text-xl mr-4 text-gray-500">PublishYear</label>
+          <label className='text-xl mr-4 text-gray-500'>PublishYear</label>
           <input
-            type="text"
-            value={publishYear}
-            onChange={(e) => setPublishYear(e.target.value)}
+            type='text'
+            name='publishYear'
+            value={formData.publishYear}
+            onChange={handleInputChange}
             className='border-2 border-gray-500 px-4 py-2 w-full'
-           />
+          />
         </div>
         <button className='p-2 bg-sky-200' onClick={handleCreateBook}>
           Create
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CreateBook
+export default CreateBook;
--- frontend/src/Pages/EditBook.jsx
+++ frontend/src/Pages/EditBook.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 import axios from 'axios';
@@ -6,9 +6,11 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { useSnackbar } from 'notistack';
 
 const EditBook = () => {
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const [publishYear, setPublishYear] = useState('');
+  const [formData, setFormData] = useState({
+    title: '',
+    author: '',
+    publishYear: '',
+  });
   const [loading, setLoading] = useState(false);
   const { enqueueSnackbar } = useSnackbar();
   const { id } = useParams();
@@ -18,78 +20,79 @@ const EditBook = () => {
     setLoading(true);
     axios
       .get(`http://localhost:8888/books/${id}`)
-      .then((response) =>{
+      .then((response) => {
         setLoading(false);
-        setTitle(response.data.title);
-        setAuthor(response.data.author);
-        setPublishYear(response.data.publishYear);
+        const { title, author, publishYear } = response.data;
+        setFormData({ title, author, publishYear });
       })
       .catch((error) => {
         console.log(error);
         setLoading(false);
+      });
+  }, [id]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleUpdateBook = () => {
+    setLoading(true);
+    axios
+      .put(`http://localhost:8888/books/${id}`, formData)
+      .then(() => {
+        setLoading(false);
+        enqueueSnackbar('Book edited successfully', { variant: 'success' });
+        navigate('/');
       })
-  }, [])
-
- const handleUpdateBook = () => {
-  const data = {
-    title,
-    author,
-    publishYear
-  }
-  setLoading(true);
-  axios
-    .put(`http://localhost:8888/books/${id}`, data)
-    .then(() => {
-      setLoading(false);
-      enqueueSnackbar('Book edited successfully', { variant: 'success'});
-      navigate('/');
-    })
-    .catch((error) => {
-      console.log(error);
-      enqueueSnackbar('Error', { variant: 'error'});
-    })
-
- }
+      .catch((error) => {
+        console.log(error);
+        enqueueSnackbar('Error', { variant: 'error' });
+      });
+  };
 
   return (
     <div className='p-4'>
       <BackButton />
-      { loading ? <Spinner /> : ''}
+      {loading && <Spinner />}
       <h1 className='text-3xl my-4 text-center'>Edit book</h1>
       <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
         <div className='my-4'>
-          <label className="text-xl mr-4 text-gray-500">Title</label>
+          <label className='text-xl mr-4 text-gray-500'>Title</label>
           <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            type='text'
+            name='title'
+            value={formData.title}
+            onChange={handleInputChange}
             className='border-2 border-gray-500 px-4 py-2 w-full'
-           />
+          />
         </div>
         <div className='my-4'>
-          <label className="text-xl mr-4 text-gray-500">Author</label>
+          <label className='text-xl mr-4 text-gray-500'>Author</label>
           <input
-            type="text"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
+            type='text'
+            name='author'
+            value={formData.author}
+            onChange={handleInputChange}
             className='border-2 border-gray-500 px-4 py-2 w-full'
-           />
+          />
         </div>
         <div className='my-4'>
-          <label className="text-xl mr-4 text-gray-500">PublishYear</label>
+          <label className='text-xl mr-4 text-gray-500'>PublishYear</label>
           <input
-            type="text"
-            value={publishYear}
-            onChange={(e) => setPublishYear(e.target.value)}
+            type='text'
+            name='publishYear'
+            value={formData.publishYear}
+            onChange={handleInputChange}
             className='border-2 border-gray-500 px-4 py-2 w-full'
-           />
+          />
         </div>
         <button className='p-2 bg-sky-200' onClick={handleUpdateBook}>
           Update Book
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default EditBook
+export default EditBook;
